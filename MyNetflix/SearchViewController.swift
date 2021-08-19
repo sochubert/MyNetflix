29392e7ab9315093878c0fcd485512e59dd8b209
sochubert <sochubert@gmail.com>
2021-08-19 23:11:38 +0900

    [SearchView] SearchBar

--- MyNetflix/SearchViewController.swift
+++ MyNetflix/SearchViewController.swift
@@ -9,9 +9,33 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var resultCollectionView: UICollectionView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+}
+
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    // Keyboard hide
+    private func dismissKeyboard() {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        dismissKeyboard()
+        // 검색어가 있는지?
+        guard let searchTerm = searchBar.text, searchTerm.isEmpty == false else { return }
+        
+        // Search by networking
+        
+        // Search start
+        print("-->\(searchTerm)")
+    }
 }
