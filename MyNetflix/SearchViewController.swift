3e8fa0afd1071b88e7f2b410c77de66fcde9631b
sochubert <sochubert@gmail.com>
2021-09-01 17:14:08 +0900

    Search result using API

--- MyNetflix/SearchViewController.swift
+++ MyNetflix/SearchViewController.swift
@@ -7,17 +7,58 @@
 //
 
 import UIKit
+import Kingfisher
 
 class SearchViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var resultCollectionView: UICollectionView!
     
+    var movies: [Movie] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
+
+extension SearchViewController: UICollectionViewDataSource {
+    // 몇개 넘어오는가
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    // 어떻게 표현?
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ResultCell", for: indexPath) as?
+        ResultCell else {
+            return UICollectionViewCell()
+        }
+        let movie = movies[indexPath.item]
+        // imagepath(string) -> image
+        // SPM으로
+        let url = URL(string: movie.thumbnailPath)!
+        cell.movieThumbnail.kf.setImage(with: url)
+        return cell
+    }
     
+    
+}
+
+extension SearchViewController: UICollectionViewDelegate {
+    
+}
+
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let margin: CGFloat = 8
+        let itemSpacing: CGFloat = 10
+        let width = (collectionView.bounds.width - margin * 2 - itemSpacing * 2) / 3
+        let height = width * 10/7
+        return CGSize(width: width, height: height)
+    }
+}
+
+class ResultCell: UICollectionViewCell {
+    @IBOutlet weak var movieThumbnail: UIImageView!
 }
 
 
@@ -38,6 +79,10 @@ extension SearchViewController: UISearchBarDelegate {
         SearchAPI.search(searchTerm) { movies in
             // collectionView 표현
             print("--> \(movies.count)")
+            DispatchQueue.main.async {
+                self.movies = movies
+                self.resultCollectionView.reloadData()
+            }
         }
         // Search start
         print("-->\(searchTerm)")
